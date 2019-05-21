20584b9222c8c600fca9a6634fc48461879a8264
DoctorChe <duncan.reg@yandex.ru>
2019-05-21 10:48:12 +0300

    correction of 8-th task

--- Lesson_2/8.py
+++ Lesson_2/8.py
@@ -16,6 +16,14 @@ def count_digit(number, my_digit):
     return count
 
 
+def loop(n, my_digit):
+    total_count = 0
+    for i in range(1, n + 1):
+        number = int(input(f"Введите {i}-е число: "))
+        total_count += count_digit(number, my_digit)
+    return total_count
+
+
 # Расчёт через рекурсию
 def count_digit_recursion(number, my_digit, count=0):
     if number:
@@ -27,11 +35,21 @@ def count_digit_recursion(number, my_digit, count=0):
         return count
 
 
-input_number = int(input(f"Введите последовательность чисел: "))
-my_digit = int(input(f"Введите цифру: "))
+def recursion(n, my_digit, i=1, total_count=0):
+    if i <= n:
+        number = int(input(f"Введите {i}-е число: "))
+        total_count += count_digit_recursion(number, my_digit)
+        i += 1
+        return recursion(n, my_digit, i, total_count)
+    else:
+        return total_count
+
+
+n = int(input(f"Введите колличество вводимых чисел: "))
+my_digit = int(input(f"Введите цифру, которую будем искать: "))
 
 print("\nРасчёт через цикл")
-print(f"Цифра {my_digit} встречается {count_digit(input_number, my_digit)} раз(а)")
+print(f"Цифра {my_digit} встречается {loop(n, my_digit)} раз(а)")
 
 print("\nРасчёт через рекурсию")
-print(f"Цифра {my_digit} встречается {count_digit_recursion(input_number, my_digit)} раз(а)")
+print(f"Цифра {my_digit} встречается {recursion(n, my_digit)} раз(а)")
