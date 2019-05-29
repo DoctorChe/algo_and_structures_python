c1b92f95310eb1a8ea3a74c08e7ab8d701d6cc50
DoctorChe <duncan.reg@yandex.ru>
2019-05-29 15:57:26 +0300

    lesson 4 is done

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,64 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+
+# 5.	В массиве найти максимальный отрицательный элемент.
+# Вывести на экран его значение и позицию (индекс) в массиве.
+
+from random import random
+import timeit
+import cProfile
+
+list_length = 1000
+max_number = 1000
+lst = [int(int(random() * max_number) - (max_number / 2)) for _ in range(list_length)]
+
+
+# Вариант 1
+def list_index(lst, el):
+    result = -1
+    index = 0
+    for lst_el in lst:
+        if el == lst_el:
+            result = index
+            break
+        index += 1
+    return result
+
+
+def fun_max_neg(max_number):
+    max_negative = -max_number
+    for el in lst:
+        if max_negative < el < 0:
+            max_negative = el
+    return max_negative
+
+
+print(timeit.timeit("fun_max_neg(max_number)",
+                    setup="from __main__ import fun_max_neg, list_index, list_length, max_number, lst",
+                    number=10000))
+
+# Вариант 2
+print(timeit.timeit("max(i for i in lst if i < 0)",
+                    setup="from __main__ import lst",
+                    number=10000))
+
+
+def main():
+    fun_max_neg(max_number)
+    max(i for i in lst if i < 0)
+
+
+cProfile.run("main()")
+
+"""
+Согласно замерам скорости первый алгоритм получился быстрее
+
+Вариант 1
+0.6291005913677955
+
+Вариант 2
+0.7705054371215858
+
+Увеличение времени второго варианта происходит вероятно из-за многократного вызова генераторного выражения 
+"""
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,70 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+
+import timeit
+import cProfile
+
+n = 10
+
+
+def eratosthenes(n):
+    max_number = 100
+    a = list(range(max_number + 1))
+    a[1] = 0
+    primes = []
+
+    i = 2
+    while len(primes) < n:
+        if a[i] != 0:
+            primes.append(a[i])
+            for j in range(i, max_number + 1, i):
+                a[j] = 0
+        i += 1
+    return primes
+
+
+def is_prime(n):
+    for j in range(3, int(n ** 0.5) + 1):
+        if n % j == 0:
+            return 0
+    return 1
+
+
+def search_primes(n):
+    primes = [2]
+    i = 3
+    while len(primes) < n:
+        if is_prime(i) == 1:
+            primes.append(i)
+        i += 2
+    return primes
+
+
+print(timeit.timeit("eratosthenes(n)",
+                    setup="from __main__ import eratosthenes, n",
+                    number=100000))
+
+# Вариант 2
+print(timeit.timeit("search_primes(n)",
+                    setup="from __main__ import search_primes, is_prime, n",
+                    number=100000))
+
+
+def main():
+    print(f"{n}-e простое число: {eratosthenes(n)[-1]} (Решето Эратосфена)")
+    print(f"{n}-e простое число: {search_primes(n)[-1]}")
+
+
+cProfile.run("main()")
+
+"""
+Согласно замерам скорости алгоритмов практически равны
+
+Вариант 1 (Решето Эратосфена)
+1.7685222373585983
+
+Вариант 2
+1.7270139381619358
+
+"""
