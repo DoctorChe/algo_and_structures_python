8a93888de57afad01a5790d6dca4a45b4991b6fa
DoctorChe <duncan.reg@yandex.ru>
2019-06-05 00:48:44 +0300

    start lesson 6

--- Lesson_6/1.py
+++ Lesson_6/1.py
@@ -7,3 +7,72 @@
 комментариев к коду. Также укажите в комментариях версию Python
 и разрядность вашей ОС.
 """
+
+# from pympler
+import sys
+# import timeit
+# import cProfile
+
+n = 10
+
+
+# Вариант 1 (Решето Эратосфена)
+def eratosthenes(n):
+    max_number = 10000
+    a = list(range(max_number + 1))
+    a[1] = 0
+    primes = []
+
+    i = 2
+    while len(primes) < n:
+        if a[i] != 0:
+            primes.append(a[i])
+            for j in range(i, max_number + 1, i):
+                a[j] = 0
+        i += 1
+    print(sys.getrefcount(1))
+    return primes
+
+
+# Вариант 2
+def is_prime(n):
+    for j in range(3, int(n ** 0.5) + 1):
+        if n % j == 0:
+            return 0
+    return 1
+
+
+def search_primes(n):
+    primes = [2]
+    i = 3
+    while len(primes) < n:
+        if is_prime(i) == 1:
+            primes.append(i)
+        i += 2
+    print(sys.getrefcount(1))
+    return primes
+
+
+# print(timeit.timeit("eratosthenes(n)",
+#                     setup="from __main__ import eratosthenes, n",
+#                     number=1000))
+#
+#
+# print(timeit.timeit("search_primes(n)",
+#                     setup="from __main__ import search_primes, is_prime, n",
+#                     number=1000))
+
+
+def main():
+    # print(f"{n}-e простое число: {eratosthenes(n)[-1]} (Решето Эратосфена)")
+    print(f"{n}-e простое число: {search_primes(n)[-1]}")
+
+
+# cProfile.run("main()")
+
+main()
+
+"""
+Python 3.6
+x64
+"""
--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,59 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+
+
+from pympler import asizeof
+
+
+class Person:
+    """Класс для сущности человек"""
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def display_information(self):
+        print(self.__class__.__name__)
+        print('name:', self.firstname, self.lastname)
+
+
+class PersonSlot:
+    """Класс для сущности человек"""
+    __slots__ = ("firstname", "lastname")
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def display_information(self):
+        print(self.__class__.__name__)
+        print('name:', self.firstname, self.lastname)
+
+
+# class MyResume:
+#     def __init__(self, name, address):
+#         self.name = name
+#         self.address = address
+#
+#
+#
+# class MyResumeSlots:
+#     __slots__ = ('name', 'address')
+#     def __init__(self, name, address):
+#         self.name = name
+#         self.address = address
+
+
+mr = Person("иван", "иванов")
+print(mr.__dict__)
+print(asizeof.asizeof(mr))
+#mr.surname = 'фамилия'
+#print(mr.__dict__)
+#print(asizeof.asizeof(mr))
+
+mrs = PersonSlot("иванов", "иванов")
+print(mrs.__slots__)
+
+#mrs.surname = 'фамилия'
+
+print(asizeof.asizeof(mr))
+print(asizeof.asizeof(mrs))
